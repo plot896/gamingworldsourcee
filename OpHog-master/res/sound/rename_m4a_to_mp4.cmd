062d9472ebdbdfcb4237062a7ad609d9be976f0e
plot86 <plotracer189@gamil.com>
2022-02-06 19:49:35 +0530

    All

--- OpHog-master/res/sound/rename_m4a_to_mp4.cmd
+++
@@ -1,2 +0,0 @@
-:: This script simply renames all .m4a files in the current directory to mp4.
-forfiles /M *.m4a /c "cmd /c del @fname.mp4 & ren @file @fname.mp4 && del @file"
--- OpHog-master/src/tools/publish.cmd
+++
@@ -1,1 +0,0 @@
-python publisher.py ..\index.html index.html "C:\Users\agd13_000\Downloads\UglifyJS-master\UglifyJS-master\bin\uglifyjs" ../src
